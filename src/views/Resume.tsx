3bc192aa8ea365d40b00c96fefa0cd9e85d5352d
Aqib Bhat <aqib.n.bhat@gmail.com>
2023-12-28 13:00:19 -0800

    Update the Certifications section of Resume with real data; Keep data separate

--- src/views/Resume.tsx
+++ src/views/Resume.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { styled } from '@mui/material/styles';
 import { Accordion, AccordionDetails, AccordionSummary, Box, CardContent, Grid, Link, Paper, Typography } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { experienceInfo, certifications } from '../data/ResumeData';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -10,37 +11,6 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
-
-const experienceInfo = [
-  {
-    name: 'BlackLine Systems Inc.',
-    location: 'Issaquah, Washington state, USA (Remote)',
-    title: 'Senior Software Engineer',
-    timePeriod: 'April 4th, 2022 - October 27th, 2023',
-    details: 'I worked in the Platform team building microservices and implementing the integration with Okta to enable a Single Sign-On (SSO) experience for the tenant users of the SaaS applications. I was also involved in improving operations, performance, and monitoring.'
-  },
-  {
-    name: 'Amazon',
-    location: 'Seattle, Washington state, USA',
-    title: 'Software Development Engineer (SDE)',
-    timePeriod: 'May 30th 2017 - April 1st 2022',
-    details: 'I was part of the Amazon Publishing division and worked on various internal and external projects (examples: Amazon Original Stories and World Book Day celebrations). I did full-stack work as well as cloud migrations of existing applications. Besides new features, I remodeled existing applications to streamline the book metadata and comply with privacy regulations.'
-  },
-  {
-    name: 'Amazon',
-    location: 'Seattle, Washington state, USA',
-    title: 'Software Development Engineer (SDE) Intern',
-    timePeriod: 'May 17th 2016 - August 5th 2016',
-    details: 'I got selected for the coveted software development summer internship at Amazon and developed a news feed for an internal website. I ended up getting a full-time job offer at the end.'
-  },
-  {
-    name: 'Wipro Technologies',
-    location: 'Pune, India and York, UK',
-    title: 'Project Engineer to Senior Software Engineer',
-    timePeriod: 'April 26th 2010 - May 27th 2015',
-    details: 'I started work as a software engineer after graduation. I learned and practiced full-stack deployment. I worked on large projects for insurance and banking clients. I also worked in the UK while handling the offshore team in India.'
-  },
-];
 
 export default function Resume() {  
     return (
@@ -140,10 +110,15 @@ export default function Resume() {
                         <Typography variant="h5">🥷 Certifications</Typography>
                       </AccordionSummary>
                       <AccordionDetails>
-                        <Typography>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                          malesuada lacus ex, sit amet blandit leo lobortis eget.
-                        </Typography>
+                        {certifications.map((item) => (
+                          <React.Fragment key={item.name}>
+                            <CardContent>
+                              <Typography variant="h6" gutterBottom align='left'>{item.name}</Typography>
+                              <Typography variant="subtitle1" gutterBottom align='left'>🏛 <strong>Issuing Authority: </strong>{item.issuingAuthority}</Typography>
+                              <Typography variant="body1" gutterBottom align='left'>🗓 <strong>Issue Date: </strong>{item.issueDate}</Typography>
+                            </CardContent>
+                          </React.Fragment>
+                        ))}
                       </AccordionDetails>
                     </Accordion>
                 </Item>
