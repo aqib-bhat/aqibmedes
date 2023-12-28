cb864cda8a6a007e8da4c3f569d58406da7fc79e
Aqib Bhat <aqib.n.bhat@gmail.com>
2023-12-28 01:29:41 -0800

    Adding grid layout to the Resume view

--- src/views/Resume.tsx
+++ src/views/Resume.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Link } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { Box, Grid, Link, Paper } from '@mui/material';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export default function Resume() {  
     return (
@@ -12,6 +21,34 @@ export default function Resume() {
             </Link><br/>
             Curriculum Vitae (CV) with detailed work history is available on request.
           </p>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={8}>
+                <Item>Breif intro</Item>
+              </Grid>
+              <Grid item xs={4}>
+                <Item>Location and Contact</Item>
+              </Grid>
+              <Grid item xs={8}>
+                <Item>Experience</Item>
+              </Grid>
+              <Grid item xs={4}>
+                <Item>Skills</Item>
+              </Grid>
+              <Grid item xs={12}>
+                <Item>Education</Item>
+              </Grid>
+              <Grid item xs={4}>
+                <Item>Certifications</Item>
+              </Grid>
+              <Grid item xs={4}>
+                <Item>Languages</Item>
+              </Grid>
+              <Grid item xs={4}>
+                <Item>Test Scores</Item>
+              </Grid>
+            </Grid>
+          </Box>
         </main>
       </div>
     );
