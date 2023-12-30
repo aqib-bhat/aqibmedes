1a58e3e9ca68e8d63e67782f7acb706637454175
Aqib Bhat <aqib.n.bhat@gmail.com>
2023-12-30 01:10:55 -0800

    Styling and organizing the About view

--- src/views/AboutMe.tsx
+++ src/views/AboutMe.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Divider, Typography } from '@mui/material';
 
 class AboutMe extends React.Component {
     render() {
@@ -7,20 +7,22 @@ class AboutMe extends React.Component {
             <div>
                 <Typography variant="h3" gutterBottom sx={{ fontWeight: 'bold' }}>About Me</Typography>
                 <main>
-                    <p>
-                        A software engineer currently based in the Greater Vancouver Area in Canada.<br/>
-                        Software Engineering has taught me a lot about how to get things done and life in general as well.<br/>
+                    <Typography variant="body1" align='left' gutterBottom>
+                        A software engineer currently based in the Greater Vancouver Area in Canada.
+                        Software Engineering has taught me a lot about how to get things done and life in general as well.
                         I am also interested in a host of other things, from science and spirituality to sports and comedy.
-                    </p>
-                    <p><strong>Brief professional bio:</strong> ABC</p>
-                    <p>
-                        Thought process<br/>
-                        Conceptual clarity | Empathy, Justice, and Logic
-                    </p>
-                    <p>Interests outside of work</p>
-                    <p>
-                        Motto: Leave things in a better place than you find them.<br/>
-                    </p>
+                    </Typography>
+                    <br/><Divider></Divider><br/>
+                    <Typography variant="subtitle1" align='left' sx={{ fontWeight: 'bold' }}>Brief professional bio</Typography>
+                    <Typography variant="body1" align='left' gutterBottom>
+                        xyz
+                    </Typography>
+                    <br/><Divider></Divider><br/>
+                    <Typography variant="subtitle1" align='left' sx={{ fontWeight: 'bold' }}>Interests outside of work</Typography>
+                    <Typography variant="body1" align='left' gutterBottom>
+                        xyz
+                    </Typography>
+                    <br/><Divider></Divider><br/>
                 </main>
             </div>
         )
