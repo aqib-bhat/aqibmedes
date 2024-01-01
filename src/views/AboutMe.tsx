0fa778a80430786c7c9318ad211b6560eabb298b
Aqib Bhat <aqib.n.bhat@gmail.com>
2024-01-01 04:38:13 -0800

    Removed unused import

--- src/views/AboutMe.tsx
+++ src/views/AboutMe.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Divider, Link, Typography } from '@mui/material';
+import { Divider, Typography } from '@mui/material';
 import { StyledRouterLink } from '../components/StyledRouterLink';
 
 class AboutMe extends React.Component {
