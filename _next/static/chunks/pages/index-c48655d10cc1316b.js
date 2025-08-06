32b5612492d6392b935e9331b02aded81a0b35d1
Aqib Niaz Bhat <aqib.n.bhat@gmail.com>
2025-08-06 01:39:16 -0700

    Updates

---
+++ _next/static/chunks/pages/index-c48655d10cc1316b.js
@@ -0,0 +1,16 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8312:function(e,n,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return t(1854)}])},1854:function(e,n,t){"use strict";t.r(n),t.d(n,{default:function(){return c}});var i=t(5893);t(7294);var s=t(990),r=t(8573),o=t(6788),a=t(7174);function c(){return(0,i.jsx)("div",{children:(0,i.jsx)(s.Z,{sx:{flexGrow:1,padding:2},children:(0,i.jsxs)(r.ZP,{container:!0,spacing:2,children:[(0,i.jsxs)(r.ZP,{item:!0,sm:12,md:8,lg:10,children:[(0,i.jsx)(o.Z,{variant:"subtitle1",children:"Howdy! I am Aqib."}),(0,i.jsx)(o.Z,{variant:"subtitle1",children:"I am a software engineer by passion and profession."}),(0,i.jsxs)(o.Z,{variant:"subtitle1",children:["To know more about me, check the"," ",(0,i.jsx)(a.s,{to:"/about",children:"About"})," page."]}),(0,i.jsxs)(o.Z,{variant:"subtitle1",children:["For professional information, check the"," ",(0,i.jsx)(a.s,{to:"/resume",children:"Resume"})," page."]}),(0,i.jsxs)(o.Z,{variant:"subtitle1",children:["To get in touch, check the"," ",(0,i.jsx)(a.s,{to:"/contact",children:"Contact"})," page."]})]}),(0,i.jsx)(r.ZP,{item:!0,sm:12,md:4,lg:2,children:(0,i.jsx)("img",{src:"/aqibmedes/Aqib_DP.jpeg",className:"App-logo",alt:"Aqib"})})]})})})}},7174:function(e,n,t){"use strict";t.d(n,{s:function(){return c}});var i=t(5893),s=t(1664),r=t.n(s),o=t(7527);let a=(0,o.ZP)("a")`
+  color: lightgreen;
+  font-weight: bold;
+  text-decoration: none;
+
+  /* added & */
+  &:visited {
+    color: lightblue;
+  }
+
+  /* added & */
+  &:hover {
+    color: yellow;
+    transition-duration: 0.3s;
+  }
+`,c=e=>{let{to:n,children:t}=e;return(0,i.jsx)(r(),{href:n,passHref:!0,legacyBehavior:!0,children:(0,i.jsx)(a,{children:t})})}}},function(e){e.O(0,[573,888,774,179],function(){return e(e.s=8312)}),_N_E=e.O()}]);
